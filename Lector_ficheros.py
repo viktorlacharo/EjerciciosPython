d36c1131e28fd0d2271abfe6adb0ba8191200aa3
Viktor Git <viktor@viktor.com>
2022-01-03 19:55:38 +0100

    Ejercicio 1 py

---
+++ Lector_ficheros.py
@@ -0,0 +1,17 @@
+
+from os import walk
+
+lista_archivos = []
+
+for (dirpath, dirnames, filenames) in walk("C:\\Users\\Vikto\\Desktop\\Openbootcamp"):
+    lista_archivos.append(filenames)
+        
+if len(lista_archivos) == 0:
+    print("No hay ficheros dentro de la dirección indicada, revisala.")
+else:
+    for file in lista_archivos[:]:
+        print(f"{file}\n")
+
+
+
+ 
